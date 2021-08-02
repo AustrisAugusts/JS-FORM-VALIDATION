b2558c3b67af16ee95d2d2ba0333baa984933bc0
Austris <biosss91@gmail.com>
2021-08-02 11:06:44 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,12 @@
 "use strict";
 
-const validateForm = function() {
-  const fName = document.forms["form"]["fname"].value;
-  const email = document.forms["form"]["email"].value;
-  const phone = document.forms["form"]["phone"].value;
-  const message = document.forms["form"]["message"].value;
-  const button = document.forms["form"]["submit"].value;
-  console.log(fName, email, phone, message);
-
-
-  button.addEventListener("click", function(){
-    // See if input is not empty
-    const notEmpty = str => !str.trim().length;
-
-    // Input field check
-    const fieldCheck = function(field) {
-      field.addEventListener("input", function() {
-        notEmpty(this.value) ? console.log(this.value) : console.log(`Please fill the ${field}`);
-      });
-    };
-    fieldCheck(fName);
-    fieldCheck(email);
-
-    console.log(`Button clicked`);
-  });
+function formValidation() {
+  let name = document.forms["form"]["fname"].value;
+  let email = document.forms["form"]["email"].value;
+  let phone = document.forms["form"]["phone"].value;
+  let message = document.forms["form"]["message"].value;
+  (name === "") ? alert(`Name field is empty`) : console.log(name);
+  (email === "") ? alert(`email field is empty`) : console.log(email);
+  (phone === "") ? alert(`phone field is empty`) : console.log(phone);
+  (message === "") ? alert(`message field is empty`) : console.log(message);
 }
